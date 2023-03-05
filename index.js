53cf7d078ca9f6c5c0fcecdeaa3b01033abfda18
Ester <bittencourt.ester@gmail.com>
2023-03-05 16:52:11 -0300

    Pagina Login ok?

---
+++ index.js
@@ -0,0 +1,16 @@
+var formLogin = document.querySelector('#Login');
+var formCadastro = document.querySelector('#cadastro');
+var btnColor = document.querySelector('.btnColor');
+
+document.querySelector('#btnLogin').addEventListener('click', () => {
+  formLogin.style.left = "25px"
+  formCadastro.style.left = "450px"
+  btnColor.style.left = "0px"
+});
+
+document.querySelector('#btnCadastro').addEventListener('click', () => {
+  formLogin.style.left = "-450px"
+  formCadastro.style.left = "25px"
+  btnColor.style.left = "110px"
+});
+
